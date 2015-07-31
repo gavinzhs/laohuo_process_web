1991d96b07c490db166e72ddc5dce7dd0b8a2c33
zhengsai <gavinzhs@gmail.com>
2015-07-31 18:31:56 +0800

    remove angular-echarts

--- app/app.js
+++ app/app.js
@@ -6,7 +6,6 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.version',
-   'angular-echarts'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
